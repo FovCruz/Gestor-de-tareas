bb0a6a77f11c9c74aa331611bc39cd31bc882de0
Jacob Vega <jacob.vega@3it.cl>
2025-08-08 18:20:51 -0400

    Se crean contenedores en el proyecto

--- frontend-tareas/src/App.vue
+++ frontend-tareas/src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { useRoute, useRouter } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
 const route = useRoute()
 const router = useRouter()
 
